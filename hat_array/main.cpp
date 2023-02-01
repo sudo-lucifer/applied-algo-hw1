d81cd3274c64cc2e230fa925407b5b5e25255d73
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-02-01 19:32:01 +0700

    change to find average

--- hat_array/main.cpp
+++ hat_array/main.cpp
@@ -14,42 +14,50 @@ static __inline__ unsigned long long rdtsc(void){
     return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32   );
 }
 
-void benchmark_push_n(int n) {
+long hat_push_one(int i) {
+        HatArray hat_array;
+        long begin = rdtsc();
+        hat_array.push(i);
+        long end = rdtsc();
+        return end - begin;
+}
+
+long hat_push_n(int n) {
+        HatArray hat_array;
+        hat_array.push(i);
+        long total = 0;
+        for (int i = 0; i < n; i++) {
+                total += hat_push_one(i);
+        }
+        return total;
+}
+
+void benchmark_push_n(int n, int test_size) {
         cout << "Start benchmarking push n elements\n";
-        ResizableArray resizable_array;
-        HatArray hat_array;
-        long begin = rdtsc();
+        long avg = 0;
         for (int i = 0; i < n; i++) {
-                resizable_array.push(i);
+                avg = hat_push_n(test_size);
         }
-        long end = rdtsc();
-        resizable_array.destroy();
-        cout << "Traditional resizable array CPU count: " << (end - begin) << "\n";
-        begin = rdtsc();
-        for (int i = 0; i < n; i++) {
-                hat_array.push(1);
-        }
-        end = rdtsc();
-        cout << "HAT array CPU count: " << (end - begin) << "\n";
+        cout << "HAT array CPU count: " << (total / n) << "\n";
         hat_array.destroy();
 
 }
 
-void benchmark_push_one() {
-        cout << "Start benchmarking push one element\n";
-        ResizableArray resizable_array;
-        HatArray hat_array;
-        long begin = rdtsc();
-        resizable_array.push(1);
-        long end = rdtsc();
-        cout << "Traditional resizable array CPU count: " << (end - begin) << "\n";
-        resizable_array.destroy();
-        begin = rdtsc();
-        hat_array.push(1);
-        end = rdtsc();
-        cout << "HAT array CPU count: " << (end - begin) << "\n";
-        hat_array.destroy();
-}
+// void benchmark_push_one() {
+//         cout << "Start benchmarking push one element\n";
+//         ResizableArray resizable_array;
+//         HatArray hat_array;
+//         long begin = rdtsc();
+//         resizable_array.push(1);
+//         long end = rdtsc();
+//         cout << "Traditional resizable array CPU count: " << (end - begin) << "\n";
+//         resizable_array.destroy();
+//         begin = rdtsc();
+//         hat_array.push(1);
+//         end = rdtsc();
+//         cout << "HAT array CPU count: " << (end - begin) << "\n";
+//         hat_array.destroy();
+// }
 
 void save_push_benchmark(int push_amount) {
         cout << "Start recording benchmark data\n";
@@ -80,8 +88,8 @@ void save_push_benchmark(int push_amount) {
 
 
 int main() {
-        benchmark_push_one();
-        cout << "=========================================================\n";
+        // benchmark_push_one();
+        // cout << "=========================================================\n";
         benchmark_push_n(10);
         cout << "=========================================================\n";
         save_push_benchmark(50);
