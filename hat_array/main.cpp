d30da9a4f3bf87e70f9d45923608e6f98b285222
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-02-01 18:39:30 +0700

    benchmark n

--- hat_array/main.cpp
+++ hat_array/main.cpp
@@ -13,8 +13,29 @@ static __inline__ unsigned long long rdtsc(void){
     return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32   );
 }
 
+void benchmark_push_n(int n) {
+        cout << "Start benchmarking push n elements\n";
+        ResizableArray resizable_array;
+        HatArray hat_array;
+        long begin = rdtsc();
+        for (int i = 0; i < n; i++) {
+                resizable_array.push(i);
+        }
+        long end = rdtsc();
+        cout << "Traditional resizable array CPU count: " << (end - begin) << "\n";
+        begin = rdtsc();
+        for (int i = 0; i < n; i++) {
+                hat_array.push(1);
+        }
+        end = rdtsc();
+        cout << "HAT array CPU count: " << (end - begin) << "\n";
+        resizable_array.destroy();
+        hat_array.destroy();
+
+}
+
 void benchmark_push_one() {
-        cout << "Start benchmarking push one element";
+        cout << "Start benchmarking push one element\n";
         ResizableArray resizable_array;
         HatArray hat_array;
         long begin = rdtsc();
@@ -33,5 +54,7 @@ void benchmark_push_one() {
 
 int main() {
         benchmark_push_one();
+        cout << "=========================================================\n";
+        benchmark_push_n(10);
         return 0;
 }
