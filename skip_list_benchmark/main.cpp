2bb580160d893dca1a05cd8add605c5ef2c1c097
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-02-06 00:05:19 +0700

    insert benchmark

--- skip_list_benchmark/main.cpp
+++ skip_list_benchmark/main.cpp
@@ -1,38 +1,77 @@
 #include <iostream>
 #include "./linked_list/linked_list.h"
 #include "math.h"
+#include <fstream>
 #include "./skip_list/skip_list.h"
 
 using namespace std;
 
-void test_skip_list() {
-	int n = 20;
+static __inline__ unsigned long long rdtsc(void){
+    unsigned hi, lo;  
+    __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
+    return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32   );
+}
+
+
+void insert_latency(int n) {
+	ofstream SkipListFile("skip_list_insert.csv");
+	ofstream LinkedListFile("linked_list_insert.csv");
+	SkipListFile << "operation,cpu_count\n";
+	LinkedListFile << "operation,cpu_count\n";
 	int max_level_skip_list = log(n);
-	SkipList a = SkipList(max_level_skip_list,(float) 1/4);
+	SkipList skip_list(max_level_skip_list, (float) 1/4);
+	LinkedList linked_list;
+	cout << "================ Insert ================\n";
+	long total_skip_list = 0;
+	long total_linked_list = 0;
 	for (int i = 1; i <= n; i++) {
-		a.insert(i);
+		long start = rdtsc();
+		linked_list.push(i);
+		long end = rdtsc();
+		LinkedListFile << i << "," << (end - start) << "\n";
 	}
-	cout << a.toString();
-	cout << a.search(21);
+	for (int i = 1; i <= n; i++) {
+		long start = rdtsc();
+		skip_list.insert(i);
+		long end = rdtsc();
+		SkipListFile << i << "," << (end - start) << "\n";
+	}
+	cout << "Average one insertion cycle count\n";
+	cout << "Skip list: " << (total_skip_list / n) << "\n";
+	cout << "linked list: " << (total_linked_list / n) << "\n";
+	cout << "\nTotal cycle cout of " << n << " insertion cycle count\n";
+	cout << "Skip list: " << total_skip_list << "\n";
+	cout << "linked list: " << total_linked_list << "\n";
 }
 
-void test_linked_list() {
-	LinkedList a;
-	a.push(1);
-	a.push(10);
-	a.push(5);
-	cout << a.get(0) << "\n";
-	cout << a.get(1) << "\n";
-	cout << a.get(2) << "\n";
-	cout << a.get(3) << "\n";
+// void test_skip_list() {
+// 	int n = 20;
+// 	int max_level_skip_list = log(n);
+// 	SkipList a = SkipList(max_level_skip_list,(float) 1/4);
+// 	for (int i = 1; i <= n; i++) {
+// 		a.insert(i);
+// 	}
+// 	cout << a.toString();
+// 	cout << a.search(21);
+// }
 
-	cout << a.search(5) << "\n";
-	cout << a.toString() << "\n";
-	a.destroy();
+// void test_linked_list() {
+// 	LinkedList a;
+// 	a.push(1);
+// 	a.push(10);
+// 	a.push(5);
+// 	cout << a.get(0) << "\n";
+// 	cout << a.get(1) << "\n";
+// 	cout << a.get(2) << "\n";
+// 	cout << a.get(3) << "\n";
 
-}
+// 	cout << a.search(6) << "\n";
+// 	cout << a.toString() << "\n";
+// 	a.destroy();
+// }
 
 int main() {
-	test_skip_list();
+	int n = 100;
+	insert_latency(n);
 	return 0;
 }
