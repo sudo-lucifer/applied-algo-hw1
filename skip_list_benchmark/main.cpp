df1b0300c9315ae22e5d5e5eb520792c4dd53ab7
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-02-06 22:31:41 +0700

    search p

--- skip_list_benchmark/main.cpp
+++ skip_list_benchmark/main.cpp
@@ -14,75 +14,75 @@ static __inline__ unsigned long long rdtsc(void){
 }
 
 
-// void insert_latency(int n) {
-// 	ofstream SkipListFile("skip_list_insert.csv");
-// 	ofstream LinkedListFile("linked_list_insert.csv");
-// 	SkipListFile << "operation,cpu_count\n";
-// 	LinkedListFile << "operation,cpu_count\n";
-// 	int max_level_skip_list = log(n);
-// 	SkipList skip_list(max_level_skip_list, (float) 1/4);
-// 	LinkedList linked_list;
-// 	cout << "================ Insert ================\n";
-// 	long total_skip_list = 0;
-// 	long total_linked_list = 0;
-// 	for (int i = 1; i <= n; i++) {
-// 		long start = rdtsc();
-// 		skip_list.insert(i);
-// 		long end = rdtsc();
-// 		SkipListFile << i << "," << (end - start) << "\n";
-// 		total_skip_list += (end - start);
-// 	}
-// 	for (int i = 1; i <= n; i++) {
-// 		long start = rdtsc();
-// 		linked_list.push(i);
-// 		long end = rdtsc();
-// 		LinkedListFile << i << "," << (end - start) << "\n";
-// 		total_linked_list += (end - start);
-// 	}
-// 	cout << "Average one insertion cycle count\n";
-// 	cout << "Skip list: " << (total_skip_list / n) << "\n";
-// 	cout << "linked list: " << (total_linked_list / n) << "\n";
-// 	cout << "\nTotal cycle cout of " << n << " insertion cycle count\n";
-// 	cout << "Skip list: " << total_skip_list << "\n";
-// 	cout << "linked list: " << total_linked_list << "\n";
-// }
+void insert_latency(int n) {
+	ofstream SkipListFile("skip_list_insert.csv");
+	ofstream LinkedListFile("linked_list_insert.csv");
+	SkipListFile << "operation,cpu_count\n";
+	LinkedListFile << "operation,cpu_count\n";
+	int max_level_skip_list = log(n);
+	SkipList skip_list(max_level_skip_list, (float) 1/4);
+	LinkedList linked_list;
+	cout << "================ Insert ================\n";
+	long total_skip_list = 0;
+	long total_linked_list = 0;
+	for (int i = 1; i <= n; i++) {
+		long start = rdtsc();
+		skip_list.insert(i);
+		long end = rdtsc();
+		SkipListFile << i << "," << (end - start) << "\n";
+		total_skip_list += (end - start);
+	}
+	for (int i = 1; i <= n; i++) {
+		long start = rdtsc();
+		linked_list.push(i);
+		long end = rdtsc();
+		LinkedListFile << i << "," << (end - start) << "\n";
+		total_linked_list += (end - start);
+	}
+	cout << "Average one insertion cycle count\n";
+	cout << "Skip list: " << (total_skip_list / n) << "\n";
+	cout << "linked list: " << (total_linked_list / n) << "\n";
+	cout << "\nTotal cycle cout of " << n << " insertion cycle count\n";
+	cout << "Skip list: " << total_skip_list << "\n";
+	cout << "linked list: " << total_linked_list << "\n";
+}
 
-// void scan_latency(int n) {
-// 	ofstream SkipListFile("skip_list_scan.csv");
-// 	ofstream LinkedListFile("linked_list_scan.csv");
-// 	SkipListFile << "operation,cpu_count\n";
-// 	LinkedListFile << "operation,cpu_count\n";
-// 	int max_level_skip_list = log(n);
-// 	SkipList skip_list(max_level_skip_list, (float) 1/4);
-// 	LinkedList linked_list;
-// 	cout << "================ Search ================\n";
-// 	long total_skip_list = 0;
-// 	long total_linked_list = 0;
-// 	for (int i = 1; i <= n; i++) {
-// 		skip_list.insert(i);
-// 		linked_list.push(i);
-// 	}
-// 	for (int i = 1; i <= n; i++) {
-// 		long start = rdtsc();
-// 		skip_list.search(i);
-// 		long end = rdtsc();
-// 		SkipListFile << i << "," << (end - start) << "\n";
-// 		total_skip_list += (end - start);
-// 	}
-// 	for (int i = 1; i <= n; i++) {
-// 		long start = rdtsc();
-// 		linked_list.search(i);
-// 		long end = rdtsc();
-// 		LinkedListFile << i << "," << (end - start) << "\n";
-// 		total_linked_list += (end - start);
-// 	}
-// 	cout << "Average one search cycle count\n";
-// 	cout << "Skip list: " << (total_skip_list / n) << "\n";
-// 	cout << "linked list: " << (total_linked_list / n) << "\n";
-// 	cout << "\nTotal scan cycle cout cycle count\n";
-// 	cout << "Skip list: " << total_skip_list << "\n";
-// 	cout << "linked list: " << total_linked_list << "\n";
-// }
+void scan_latency(int n) {
+	ofstream SkipListFile("skip_list_scan.csv");
+	ofstream LinkedListFile("linked_list_scan.csv");
+	SkipListFile << "operation,cpu_count\n";
+	LinkedListFile << "operation,cpu_count\n";
+	int max_level_skip_list = log(n);
+	SkipList skip_list(max_level_skip_list, (float) 1/4);
+	LinkedList linked_list;
+	cout << "================ Search ================\n";
+	long total_skip_list = 0;
+	long total_linked_list = 0;
+	for (int i = 1; i <= n; i++) {
+		skip_list.insert(i);
+		linked_list.push(i);
+	}
+	for (int i = 1; i <= n; i++) {
+		long start = rdtsc();
+		skip_list.search(i);
+		long end = rdtsc();
+		SkipListFile << i << "," << (end - start) << "\n";
+		total_skip_list += (end - start);
+	}
+	for (int i = 1; i <= n; i++) {
+		long start = rdtsc();
+		linked_list.search(i);
+		long end = rdtsc();
+		LinkedListFile << i << "," << (end - start) << "\n";
+		total_linked_list += (end - start);
+	}
+	cout << "Average one search cycle count\n";
+	cout << "Skip list: " << (total_skip_list / n) << "\n";
+	cout << "linked list: " << (total_linked_list / n) << "\n";
+	cout << "\nTotal scan cycle cout cycle count\n";
+	cout << "Skip list: " << total_skip_list << "\n";
+	cout << "linked list: " << total_linked_list << "\n";
+}
 
 void scan_v2_latency(int n) {
 	ofstream SkipListFile("skip_list_scan.csv");
@@ -121,50 +121,75 @@ void scan_v2_latency(int n) {
 	cout << "Skip list v2: " << total_skip_list_v2 << "\n";
 }
 
-// void test_skip_list() {
-// 	int n = 30;
-// 	int max_level_skip_list = log(n);
-// 	SkipList a = SkipList(max_level_skip_list,(float) 1/2);
-// 	for (int i = 1; i <= n; i++) {
-// 		a.insert(i);
-// 	}
-// 	cout << a.toString();
-// 	cout << a.search(2);
-// }
+void test_skip_list() {
+	int n = 30;
+	int max_level_skip_list = log(n);
+	SkipList a = SkipList(max_level_skip_list,(float) 1/2);
+	for (int i = 1; i <= n; i++) {
+		a.insert(i);
+	}
+	cout << a.toString();
+	cout << a.search(2);
+}
 
-// void test_linked_list() {
-// 	LinkedList a;
-// 	a.push(1);
-// 	a.push(10);
-// 	a.push(5);
-// 	cout << a.get(0) << "\n";
-// 	cout << a.get(1) << "\n";
-// 	cout << a.get(2) << "\n";
-// 	cout << a.get(3) << "\n";
+void test_linked_list() {
+	LinkedList a;
+	a.push(1);
+	a.push(10);
+	a.push(5);
+	cout << a.get(0) << "\n";
+	cout << a.get(1) << "\n";
+	cout << a.get(2) << "\n";
+	cout << a.get(3) << "\n";
 
-// 	cout << a.search(6) << "\n";
-// 	cout << a.toString() << "\n";
-// 	a.destroy();
-// }
+	cout << a.search(6) << "\n";
+	cout << a.toString() << "\n";
+	a.destroy();
+}
 
-// void test_skip_list_v2() {
-// 	int n = 100;
-// 	int max_level_skip_list = 10;
-// 	SkipListV2 a(max_level_skip_list, (float) 1/2);
-// 	for (int i = 1; i <= n; i++) {
-// 		a.insert(i);
-// 	}
-// 	cout << a.toString();
-// 	cout << "\n";
-// 	SkipListV2Node* test = a.search(97);
-// 	cout << test->key << "\n";
-// }
+void test_skip_list_v2() {
+	int n = 100;
+	int max_level_skip_list = 10;
+	SkipListV2 a(max_level_skip_list, (float) 1/2);
+	for (int i = 1; i <= n; i++) {
+		a.insert(i);
+	}
+	cout << a.toString();
+	cout << "\n";
+	SkipListV2Node* test = a.search(97);
+	cout << test->key << "\n";
+}
+
+void best_p_for_search(int n) {
+	ofstream SkipListFile("best_p_for_search.csv");
+	SkipListFile << "p,cpu_count\n";
+	int max_level_skip_list = log2(n);
+	SkipList skip_list(max_level_skip_list, (float) 1/4);
+	cout << "================ Best p ================\n";
+	for (float p = 0; p <= 1; p += 1/8) {
+		long total_skip_list = 0;
+		SkipList skip_list(max_level_skip_list, (float) p);
+		for (int i = 1; i <= n; i++) {
+			skip_list.insert(i);
+		}
+		for (int i = 1; i <= n; i++){
+			long start = rdtsc();
+			skip_list.search(i);
+			long end = rdtsc();
+			total_skip_list += (end - start);
+		}
+		SkipListFile << p << "," << (total_skip_list / n) << "\n";
+		cout << "p = " << p << ": avevage search cpu cycle count: " <<  (total_skip_list / n) << "\n";
+	}
+	SkipListFile.close();
+}
 
 int main() {
-	int n = 10000;
+	int n = 100;
 	// insert_latency(n);
 	// scan_latency(n);
 	// test_skip_list_v2();
-	scan_v2_latency(n);
+	// scan_v2_latency(n);
+	best_p_for_search(n);
 	return 0;
 }
